8aea51056df2ea3ae7c6995a5758b2d03d268380
gkseogus <fbznffldj998@naver.com>
2022-01-24 14:32:44 +0900

    220124_최적화업데이트

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
@@ -10,14 +12,33 @@ TodoListItem으로 이루어진 배열로 변환하여 렌더링
 */
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+    const rowRenderer = useCallback(
+        ({index, key, style}) => {
+            const todo = todos[index];
+            return(
+                <TodoListItem
+                todo = {todo}
+                key = {key}
+                onRemove={onRemove}
+                onToggle={onToggle}
+                style={style}
+                />
+            );
+        },
+        [onRemove, onToggle, todos],
+    )
     return ( 
-        <div className='TodoList'>
-            {todos.map(todo => (
-                // props로 받아 온 onRemove 함수를 TodoListItem에 전달
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}  />        
-                ))}
-        </div>
+       <List
+           className="TodoList"
+           width={512} // 전체 크기
+           height={512} // 전체 높이
+           rowCount={todos.length} // 항목 갯수
+           rowHeight={57} // 항목 높이
+           rowRenderer={rowRenderer} // 항목을 렌더링할때 쓰는 함수
+           list={todos} // 배열
+           style={{outline: 'none'}} // list에 기본 적용되는 outline 스타일 제거
+       />
     );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
