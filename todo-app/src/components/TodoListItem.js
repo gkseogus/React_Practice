e9535eb094eca41b7a9f76d9ad5b6a97ced3a76e
gkseogus <fbznffldj998@naver.com>
2022-01-24 11:15:45 +0900

    220124_TodoListItem업데이트

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 import { 
     MdCheckBoxOutlineBlank,
     MdCheckBox, // 나중에 할 일이 완료될때 체크된 상태를 보여주기 위함
@@ -8,12 +9,13 @@ import './TodoListItem.scss';
 
 // 각 할 일 항목에 대한 정보를 보여주는 컴포넌트
 // todo 객체를 props로 받아와서 상태에 따라 다른 스타일 ui를 보여줌
-const TodoListItem = () => {
+const TodoListItem = ( { todo }) => {
+    const { text, checked } = todo;
     return (
         <div className='TodoListItem'>
-            <div className='checkbox'>
-                <MdCheckBoxOutlineBlank />
-                <div className='text'>할 일</div>
+            <div className={cn('checkbox', { checked })}>
+                {checked ? <MdCheckBox />: <MdCheckBoxOutlineBlank />}
+                <div className='text'>{text}</div>
             </div>
             <div className='remove'>
                 <MdRemoveCircleOutline/>
