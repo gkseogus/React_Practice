6445101743488fe5b9b5dac729410c48637aee47
gkseogus <fbznffldj998@naver.com>
2022-01-24 14:33:00 +0900

    220124_최적화업데이트2

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -9,22 +9,28 @@ import './TodoListItem.scss';
 
 // 각 할 일 항목에 대한 정보를 보여주는 컴포넌트
 // todo 객체를 props로 받아와서 상태에 따라 다른 스타일 ui를 보여줌
-const TodoListItem = ( { todo, onRemove, onToggle }) => {
+const TodoListItem = ( { todo, onRemove, onToggle, style }) => {
     const { id, text, checked } = todo;
 
     return (
-        <div className='TodoListItem'>
-            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-                {checked ? <MdCheckBox />: <MdCheckBoxOutlineBlank />}
-                <div className='text'>{text}</div>
-            </div>
+        // virtulaized 클래스로 감싼 이유는 컴포넌트 사이사이에 테두리 쳐주고
+        // 홀수, 짝수 번째 항목에 다른 배경 색상을 설정하기 위해서다
+        <div className='TodoListItem-virtualized' style={style}>
+            <div className='TodoListItem'>
+                <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+                    {checked ? <MdCheckBox />: <MdCheckBoxOutlineBlank />}
+                    <div className='text'>{text}</div>
+                </div>
 
-             {/*  버튼을 누르면 id 삭제 함수 호출 (id 값 넣음) */}
-            <div className='remove' onClick={() => onRemove(id)}>
-                <MdRemoveCircleOutline/>
+                {/*  버튼을 누르면 id 삭제 함수 호출 (id 값 넣음) */}
+                <div className='remove' onClick={() => onRemove(id)}>
+                    <MdRemoveCircleOutline/>
+                </div>
             </div>
         </div>
     );
 };
 
-export default TodoListItem;
+export default React.memo(
+    TodoListItem,
+    (prevProps, nextProps) => prevProps.todo === nextProps.todo,);
