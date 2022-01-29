fdd87009eef9c50a779f7e450e63c1695c523348
gkseogus <fbznffldj998@naver.com>
2022-01-29 16:53:24 +0900

    220129_newAdd업데이트

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import NewsPage from './pages/NewsPage';
 
-
+// /:category? : 맨 뒤에 물음표는 값이 선택적 이라는 의미 (있거나 없거나)
 const App = () => {
   return <Route path="/:category?" component={NewsPage} />;
 };
