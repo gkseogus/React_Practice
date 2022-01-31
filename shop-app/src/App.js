546b939b484d12febd494a348e3472d8ef727751
gkseogus <fbznffldj998@naver.com>
2022-01-31 16:15:34 +0900

    220131_데이터바인딩&컴포넌트

--- shop-app/src/App.js
+++ shop-app/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import shoesData from './data.js';
 
 function App() {
+  const [shoes, shoesChange] = useState([shoesData]);
+
   return (
     <div className="App">
       <>
         <Navbar bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#features">Features</Nav.Link>
@@ -19,44 +23,37 @@ function App() {
 
       <div class="container-fluid bg-dark text-light p-5">
         <div class="container bg-dark p-5">
-          <h1 class="display-4">shoping</h1>
-          <p>Go to My Website</p>
+          <h1 class="display-4">최상의품질</h1>
+          <p>Buy it right away</p>
         </div>
       </div>
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              width="100%"
-              alt="shoes"
-            />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width="100%"
-              alt="shoes"
-            />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width="100%"
-              alt="shoes"
-            />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
+          {shoes.Map((i) => {
+            return <Card shoes={shoes[i]} />;
+          })}
         </div>
       </div>
     </div>
   );
 }
 
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'
+        }
+        width="100%"
+        alt="shoes"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>
+        {props.shoes.content} & {props.shoes.price}
+      </p>
+    </div>
+  );
+}
 export default App;
