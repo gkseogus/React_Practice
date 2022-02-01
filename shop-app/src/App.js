a4fad3054a6d7712c169ab38476add88b9647cdb
gkseogus <fbznffldj998@naver.com>
2022-02-01 19:07:11 +0900

    220201_라우터기능 & 상세페이지

--- shop-app/src/App.js
+++ shop-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from 'react';
 import Data from './data.js';
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   let [shoes, shoesChange] = useState([Data]);
@@ -21,20 +22,42 @@ function App() {
         </Navbar>
       </>
 
-      <div class="container-fluid bg-dark text-light p-5">
-        <div class="container bg-dark p-5">
-          <h1 class="display-4">최상의품질</h1>
-          <p>Buy it right away</p>
+      {/* 라우팅 */}
+      <Route exact path="/">
+        {/* 점보 */}
+        <div class="container-fluid bg-dark text-light p-5">
+          <div class="container bg-dark p-5">
+            <h1 class="display-4">최상의품질</h1>
+            <p>Buy it right away</p>
+          </div>
         </div>
-      </div>
+        <div className="container">
+          <div className="row">
+            {shoes.map((a, i) => {
+              return <Card shoes={shoes[i]} />;
+            })}
+          </div>
+        </div>
+      </Route>
 
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => {
-            return <Card shoes={shoes[i]} />;
-          })}
+      <Route exact path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src="https://codingapple1.github.io/shop/shoes1.jpg"
+                width="100%"
+              />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button>
+            </div>
+          </div>
         </div>
-      </div>
+      </Route>
     </div>
   );
 }
