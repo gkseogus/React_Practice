c021d430070d94a8692b6bef07305b7126430f7d
gkseogus <fbznffldj998@naver.com>
2022-02-02 14:01:55 +0900

    220202_상세페이지

--- shop-app/src/App.js
+++ shop-app/src/App.js
@@ -3,6 +3,7 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from 'react';
 import Data from './data.js';
 import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './Detail';
 
 function App() {
   let [shoes, shoesChange] = useState([Data]);
@@ -14,50 +15,45 @@ function App() {
           <Container>
             <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Detail">Detail</Link>
+              </Nav.Link>
+              <Nav.Link>Pricing</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
       </>
 
-      {/* 라우팅 */}
-      <Route exact path="/">
-        {/* 점보 */}
-        <div class="container-fluid bg-dark text-light p-5">
-          <div class="container bg-dark p-5">
-            <h1 class="display-4">최상의품질</h1>
-            <p>Buy it right away</p>
+      <Switch>
+        {/* 라우팅 */}
+        <Route exact path="/">
+          {/* 점보 */}
+          <div class="container-fluid bg-dark text-light p-5">
+            <div class="container bg-dark p-5">
+              <h1 class="display-4">최상의품질</h1>
+              <p>Buy it right away</p>
+            </div>
           </div>
-        </div>
-        <div className="container">
-          <div className="row">
-            {shoes.map((a, i) => {
-              return <Card shoes={shoes[i]} />;
-            })}
+          <div className="container">
+            <div className="row">
+              {shoes.map((a, i) => {
+                return <Card shoes={shoes[i]} />;
+              })}
+            </div>
           </div>
-        </div>
-      </Route>
+        </Route>
 
-      <Route exact path="/detail">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img
-                src="https://codingapple1.github.io/shop/shoes1.jpg"
-                width="100%"
-              />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button>
-            </div>
-          </div>
-        </div>
-      </Route>
+        <Route exact path="/detail">
+          <Detail />
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
