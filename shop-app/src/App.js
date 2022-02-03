860992aa6029f925541e8b28a77969b8947a208e
gkseogus <fbznffldj998@naver.com>
2022-02-03 09:35:27 +0900

    220203_axios사용

--- shop-app/src/App.js
+++ shop-app/src/App.js
@@ -4,9 +4,10 @@ import { useState } from 'react';
 import Data from './data';
 import { Link, Route, Switch } from 'react-router-dom';
 import Detail from './Detail';
+import axios from 'axios';
 
 function App() {
-  let [shoes] = useState(Data);
+  let [shoes,shoesChange] = useState(Data);
 
   return (
     <div className="App">
@@ -39,9 +40,22 @@ function App() {
           <div className="container">
             <div className="row">
               {shoes.map((a, i) => {
-                return <Card shoes={shoes[i]} />;
+                return <Card shoes={shoes[i]} i={i} key={i} />;
               })}
             </div>
+            <button className='btn btn-primary' onClick={()=>{
+              
+              // get 요청, axios는 알아서 JSON을 Object로 바꿔준다.
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((result)=>{
+                // ajax로 가져온 자료 출력
+                console.log(result.data);
+                shoesChange([...shoes, ...result.data]);
+              })
+              .catch(()=>{
+                console.log('err');
+              })
+            }}>더보기</button>
           </div>
         </Route>
 
