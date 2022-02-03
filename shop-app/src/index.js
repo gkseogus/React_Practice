feb4ccf7855e72149c719e995fabf513a006b305
gkseogus <fbznffldj998@naver.com>
2022-02-03 11:10:18 +0900

    220203_redux적용

--- shop-app/src/index.js
+++ shop-app/src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import { createStore } from 'redux';
 
+// state 초기값
+let store = createStore(()=>{
+  return [{id:0, name: '신발', quan: 2}] // quan : 수량
+});
+
+// provider 로 감싼 모든 컴포넌트들은 같은 state를 공유한다.
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
