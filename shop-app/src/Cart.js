00fba7968358582c4cc895f70a419f0f9c56ced0
gkseogus <fbznffldj998@naver.com>
2022-02-07 18:18:55 +0900

    220207_update

--- shop-app/src/Cart.js
+++ shop-app/src/Cart.js
@@ -1,55 +1,68 @@
 import React from 'react';
-import {Table} from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import { connect, useDispatch, useSelector } from 'react-redux';
 
 function Cart(props) {
-    
-    // 이때 state는 redux에 있는 모든 state
-    let state = useSelector((state)=>state.reducer);
-    console.log(state)
-    let dispatch = useDispatch();
+  // 이때 state는 redux에 있는 모든 state
+  let state = useSelector((state) => state.reducer);
+  console.log(state.reducer);
+  let dispatch = useDispatch();
 
-    return (
-        <div>
-<Table striped bordered hover variant="dark">
-    <tr>
-      <th>상품명</th>
-      <th>수량</th>
-      <th>변경</th>
-    </tr>
-    {
-        state.reducer.map((a,i)=>{
-            return (
-                <tr key={i}>
-                <td>{a.id}</td>
-                <td>{a.name}</td>
-                <td>{a.quan}</td>
-                <td><button onClick={()=>{dispatch({type: '수량증가', payload : {name: 'han'}})}}>+</button>
-                <button onClick={()=>{dispatch({type: '수량감소'})}}>-</button>
-                </td>
-              </tr>
-            )
-        })
-    }
-  </Table>
-    {
-        props.alert열렸니 === true
-        ? 
-        (<div className='my-alert2'>
-          <button onClick={() => {props.dispatch({type: '닫기'})}}>닫기</button>
-        </div>) 
-        : null       
-    }
-</div>
-    )
+  return (
+    <div>
+      <Table striped bordered hover variant="dark">
+        <tr>
+          <th>상품명</th>
+          <th>수량</th>
+          <th>변경</th>
+        </tr>
+        {state.reducer.map((a, i) => {
+          return (
+            <tr key={i}>
+              <td>{a.id}</td>
+              <td>{a.name}</td>
+              <td>{a.quan}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    dispatch({ type: '수량증가' });
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  onClick={() => {
+                    dispatch({ type: '수량감소' });
+                  }}
+                >
+                  -
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </Table>
+      {props.alert열렸니 === true ? (
+        <div className="my-alert2">
+          <button
+            onClick={() => {
+              props.dispatch({ type: '닫기' });
+            }}
+          >
+            닫기
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 // redux store의 데이터를 가져와서 props로 변환
 function stateProps(state) {
-    return {
-        state: state.reducer,
-        alert열렸니 : state.reducer2
-    }
+  return {
+    state: state.reducer,
+    alert열렸니: state.reducer2,
+  };
 }
 
-export default connect(stateProps)(Cart)
+export default connect(stateProps)(Cart);
